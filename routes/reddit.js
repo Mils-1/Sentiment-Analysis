d31e9250b763c9fd8f348c8cc8cdc84ad8bbfe51
Nick Milazzo <nmils820@gmail.com>
2018-08-13 15:44:10 -0400

    Cleaned up master

--- routes/reddit.js
+++ routes/reddit.js
@@ -30,19 +30,6 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-const twoLevels = (arr) => {
-  const returnArr = [];
-  arr.forEach(item => {
-    if (item.body) returnArr.push(item.body);
-    if (item.replies) {
-      item.replies.forEach(nestedItem => {
-        if (nestedItem.body) returnArr.push(nestedItem.body);
-      })
-    }
-  })
-  return returnArr;
-}
-
 const createCommentArr = (arr) => {
   let commentArr = [];
   arr.forEach(elem => {
@@ -56,8 +43,6 @@ const createCommentArr = (arr) => {
   return commentArr;
 }
 
-const createTopLevelCommentArr = (arr) => arr.map(item => item.body);
-
 router.get('/e', async (req, res, next) => {
   try {
     const data = await r
@@ -65,11 +50,6 @@ router.get('/e', async (req, res, next) => {
       .expandReplies({ limit: 500, depth: 1 })
       .comments;
     const commentArr = createCommentArr(data);
-    const topLevel = createTopLevelCommentArr(data);
-    const twoLevelss = twoLevels(data);
-    console.log('1', commentArr.length);
-    console.log('2', topLevel.length);
-    console.log('3', twoLevelss.length);
     res.send(commentArr);
   } catch (err) {
     next(err);
