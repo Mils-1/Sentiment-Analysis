2999605d76f2d4e6a9202469655146a3afc6bbd0
Nick Milazzo <nmils820@gmail.com>
2018-08-13 15:41:48 -0400

    Wrote 3 different methods to convert raw json to comment text arr

--- routes/reddit.js
+++ routes/reddit.js
@@ -30,7 +30,7 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-const createCommentArr = (arr) => {
+const twoLevels = (arr) => {
   const returnArr = [];
   arr.forEach(item => {
     if (item.body) returnArr.push(item.body);
@@ -43,17 +43,17 @@ const createCommentArr = (arr) => {
   return returnArr;
 }
 
-const createCommentStr = (arr) => {
-  let commentStr = '';
+const createCommentArr = (arr) => {
+  let commentArr = [];
   arr.forEach(elem => {
     if (elem.body !== '') {
-      commentStr += elem.body;
+      commentArr.push(elem.body);
     }
-    if (elem.replies) {
-      commentStr += createCommentStr(elem.replies)
+    if (elem.replies.length) {
+      commentArr.push(...createCommentArr(elem.replies));
     }
   })
-  return commentStr;
+  return commentArr;
 }
 
 const createTopLevelCommentArr = (arr) => arr.map(item => item.body);
@@ -61,12 +61,15 @@ const createTopLevelCommentArr = (arr) => arr.map(item => item.body);
 router.get('/e', async (req, res, next) => {
   try {
     const data = await r
-      .getSubmission('967tme')
+      .getSubmission('96z0n0')
       .expandReplies({ limit: 500, depth: 1 })
       .comments;
-    const x = createCommentStr(data);   //this line causes a 500 internal server error
-    console.log(`x: `, x);
     const commentArr = createCommentArr(data);
+    const topLevel = createTopLevelCommentArr(data);
+    const twoLevelss = twoLevels(data);
+    console.log('1', commentArr.length);
+    console.log('2', topLevel.length);
+    console.log('3', twoLevelss.length);
     res.send(commentArr);
   } catch (err) {
     next(err);
